d3f37e56d8133ddbe12e5f8ae9619d8674b05705
Joe woods <jomidl@outlook.com>
2022-11-29 17:40:44 -0500

    adding controller v2

--- app/controllers/nvts_controller.rb
+++ app/controllers/nvts_controller.rb
@@ -1,23 +1,39 @@
 class NvtsController < ApplicationController
+  before_action :set_nvt, only: %i[show update]
 
   def index
     @nvts = Nvt.all
   end
 
   def show
-    @nvt = nvt.id
   end
 
   def new
-
+    @nvt = Nvt.new
   end
 
   def create
-
+    @nvt = Nvt.new(nvt_params)
+    if @nvt.save
+      redirect_to profile_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
+    @nvt.update(nvt_params)
+    redirect_to profile_path
+  end
 
+  private
+
+  def set_nvt
+    @nvt = Nvt.find(params[:id])
+  end
+
+  def nvt_params
+    params_require(:nvt).permit(:title, :content, :picture, :category, :date, :price)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
     resources :transactions
   end
 
+  get '/profile', to: 'pages#profile', as: 'profile'
+
 end
